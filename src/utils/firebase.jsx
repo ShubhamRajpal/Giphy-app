90e0a011ec1bb52f1177499ad43d9aea93b16a89
ShubhamRajpal <rajpal.shubham13@gmail.com>
2025-08-31 20:47:53 +0530

    Updated env for Firebase API Key

--- src/utils/firebase.jsx
+++ src/utils/firebase.jsx
@@ -3,16 +3,6 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAre4aF2ii_XZiNKgD31HO5c0PonedhmPo",
-//   authDomain: "giffyauthhentication.firebaseapp.com",
-//   projectId: "giffyauthhentication",
-//   storageBucket: "giffyauthhentication.firebasestorage.app",
-//   messagingSenderId: "269453059801",
-//   appId: "1:269453059801:web:70fbd466d47e405ddec0c8",
-//   measurementId: "G-HK1HJ8109V"
-// };
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
